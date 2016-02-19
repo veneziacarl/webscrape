b955ff298ddc1c499d48e2202a7809f2f6440743
Carl Venezia <veneziacarl@gmail.com>
2016-02-19 11:38:43 -0800

    correctly scrapes for all FatherEarth posts and dumps in to results.md

--- noko.rb
+++ noko.rb
@@ -6,7 +6,9 @@ require 'pry'
   doc = Nokogiri::HTML(open("https://www.icmag.com/ic/showthread.php?t=289990&page=#{page}"))
   doc.css('a.bigusername').each do |link|
     if link.children[0].content == "FatherEarth"
-      puts link.parent.parent.next_element.css('div')[0]
+      open('results.md', 'a') do |f|
+        f.puts link.parent.parent.next_element.css('div')[0]
+      end
     end
   end
 end
